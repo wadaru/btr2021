ddc649f338752e79c17e8dc137068ead3db47851
Wataru UEMURA <wataru@rins.ryukoku.ac.jp>
2021-11-25 12:59:52 +0900

    this script is used for cobotta's initialization on the cobotta.

---
+++ rcll2021/scripts/power.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export DISPLAY=:0
+export ROS_MASTER_URI=http://10.42.0.1:11311
+sudo modprobe denso_cobotta io_power_mode=1
+sudo chown cobotta:cobotta /dev/denso_cobotta
+roslaunch denso_cobotta_bringup denso_cobotta_bringup.launch
